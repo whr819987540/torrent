07fbbcb0bbf8acc50fdca12ab1d6f0522cf6608d
whr819987540 <819987540@qq.com>
2023-12-06 16:10:47 +0800

    config the port of distributed-learning-level server

--- config/config.go
+++ config/config.go
@@ -14,6 +14,7 @@ import (
 type Config struct {
 	Server struct {
 		ServerIP string `json:"ServerIP"`
+		ServerPort int `json:"ServerPort"`
 	} `json:"server"`
 	Client struct {
 		TotalPeers int   `json:"TotalPeers"`
